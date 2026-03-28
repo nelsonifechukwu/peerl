be5c3dcc55b7187c4b61bfd6ecbff6e41ac609a9
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-03-28 23:33:09 +0000

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="Peer Learning Platform")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # React dev servers
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
